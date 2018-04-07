7046ffe82f302069c6a75ded36b50a60b0c472f9
Marcelo Eduardo R <marceloer2011@gmail.com>
2018-04-07 11:02:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ module.exports = class Calculator{
 	print(a,b){
 		return String("a = "+a, "b = "+b);
 	}
-
-	sum(a,b){
-		return String("A soma a+b e ",+a + +b);
-	}// implementar as outras funções aqui
+	
+	// implementar as outras funções aqui
 }
 
